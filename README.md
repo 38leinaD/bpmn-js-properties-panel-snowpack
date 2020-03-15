b53c427d5040cdd7ff6b31356ff5ccebd20f6c5b
Daniel Platz <daniel.platz@me.com>
2020-03-15 09:44:00 +0100

    wip

---
+++ README.md
@@ -0,0 +1,20 @@
+# bpmn-js Modeler + Properties Panel Example
+
+This is work in progress to get the [bpmn-js](https://github.com/bpmn-io/bpmn-js) Modeller and [bpmn-js-properties-panel](https://github.com/bpmn-io/bpmn-js-properties-panel) working with [snockpack](https://www.snowpack.dev/#snowpack).
+The idea is to use the developmemt-approach snowpack offers to not require a bundling-step via e.g. webpack on each change.
+
+## Usage
+
+Run below command to install all dependencies as ES6 Modules.
+
+```
+npm install
+```
+
+Run below command to serve the webapp via browser-sync.
+
+```
+npm start
+```
+
+Please note that the is example is not currently working and still work-in-progress.
---
+++ src/index.js
@@ -0,0 +1,166 @@
+import $ from '/jquery.js';
+import BpmnModeler from '/bpmn-js/lib/Modeler.js';
+
+import PropertiesPanel from '/bpmn-js-properties-panel/lib/index.js';
+import BpmnPropertiesProvider from '/bpmn-js-properties-panel/lib/provider/bpmn/index.js';
+//import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda';
+
+import {
+  debounce
+} from '/min-dash.js';
+
+var container = $('#js-drop-zone');
+
+var canvas = $('#js-canvas');
+
+var bpmnModeler = new BpmnModeler({
+  container: canvas,
+  propertiesPanel: {
+    parent: '#js-properties-panel'
+  },
+  additionalModules: [
+    PropertiesPanel,
+    BpmnPropertiesProvider
+  ],
+  moddleExtensions: {
+    //camunda: camundaModdleDescriptor
+  }
+});
+container.removeClass('with-diagram');
+
+function createNewDiagram() {
+  fetch('/assets/pizza-collaboration.bpmn')
+    .then(response => {
+      return response.text()
+    })
+    .then(diagramXML => {
+      openDiagram(diagramXML)
+    });
+}
+
+function openDiagram(xml) {
+
+  bpmnModeler.importXML(xml, function(err) {
+
+    if (err) {
+      container
+        .removeClass('with-diagram')
+        .addClass('with-error');
+
+      container.find('.error pre').text(err.message);
+
+      console.error(err);
+    } else {
+      container
+        .removeClass('with-error')
+        .addClass('with-diagram');
+    }
+
+
+  });
+}
+
+function saveSVG(done) {
+  bpmnModeler.saveSVG(done);
+}
+
+function saveDiagram(done) {
+
+  bpmnModeler.saveXML({ format: true }, function(err, xml) {
+    done(err, xml);
+  });
+}
+
+function registerFileDrop(container, callback) {
+
+  function handleFileSelect(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var files = e.dataTransfer.files;
+
+    var file = files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+
+      var xml = e.target.result;
+
+      callback(xml);
+    };
+
+    reader.readAsText(file);
+  }
+
+  function handleDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  }
+
+  container.get(0).addEventListener('dragover', handleDragOver, false);
+  container.get(0).addEventListener('drop', handleFileSelect, false);
+}
+
+
+////// file drag / drop ///////////////////////
+
+// check file api availability
+if (!window.FileList || !window.FileReader) {
+  window.alert(
+    'Looks like you use an older browser that does not support drag and drop. ' +
+    'Try using Chrome, Firefox or the Internet Explorer > 10.');
+} else {
+  registerFileDrop(container, openDiagram);
+}
+
+// bootstrap diagram functions
+
+$(function() {
+  // Just directly load empty diagram
+  createNewDiagram();
+  $('#js-create-diagram').click(function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    createNewDiagram();
+  });
+
+  var downloadLink = $('#js-download-diagram');
+  var downloadSvgLink = $('#js-download-svg');
+
+  $('.buttons a').click(function(e) {
+    if (!$(this).is('.active')) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  });
+
+  function setEncoded(link, name, data) {
+    var encodedData = encodeURIComponent(data);
+
+    if (data) {
+      link.addClass('active').attr({
+        'href': 'data:application/bpmn20-xml;charset=UTF-8,' + encodedData,
+        'download': name
+      });
+    } else {
+      link.removeClass('active');
+    }
+  }
+
+  var exportArtifacts = debounce(function() {
+
+    saveSVG(function(err, svg) {
+      setEncoded(downloadSvgLink, 'diagram.svg', err ? null : svg);
+    });
+
+    saveDiagram(function(err, xml) {
+      setEncoded(downloadLink, 'diagram.bpmn', err ? null : xml);
+    });
+  }, 500);
+
+  bpmnModeler.on('commandStack.changed', exportArtifacts);
+});
